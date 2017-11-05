ab0d34d25adb4d5a559719e5dc68f2a733393458
BenjaminLongobardi <Benjamin.Longobardi@gmail.com>
2017-11-05 17:54:11 -0500

    Added tournament functionality

--- src/Referee.java
+++ src/Referee.java
@@ -9,19 +9,35 @@ public class Referee {
             PlayerInterface stub = (PlayerInterface) Naming.lookup("rmi://localhost:5000/rockpaperscissors1");
             PlayerInterface stub1 = (PlayerInterface) Naming.lookup("rmi://localhost:5000/rockpaperscissors2");
 
+            System.out.println("Welcome Referee, enter how many rounds of RPS to play");
+            Scanner keyboard = new Scanner(System.in);
+            int rounds = keyboard.nextInt();
+            int p1counter = 0;
+            int p2counter = 0;
+            for(int i=0; i<rounds; i++) {
 
+                int p1 = stub.shoot();
+                int p2 = stub1.shoot();
 
-            int p1 = stub.shoot();
-            int p2 = stub1.shoot();
-
-            if(p1>p2){
-                System.out.println("Player 1 wins");
+                if (p1 > p2) {
+                    System.out.println("Player 1 wins this round");
+                    p1counter = p1counter +1;
+                } else if (p1 == p2) {
+                    System.out.println("draw");
+                } else {
+                    System.out.println("Player 2 wins this round");
+                    p2counter = p2counter +1;
+                }
             }
-            else if(p1==p2){
-                System.out.println("draw");
+            if(p1counter>p2counter){
+                System.out.println("P1 Wins!");
             }
-            else
-                System.out.println("Player 2 wins");
+            else if(p1counter<p2counter){
+                System.out.println("P2 Wins!");
+            }
+            else{
+                System.out.println("The tournament is a draw");
+            }
         }catch (Exception e){System.out.println(e);}
     }
 }
